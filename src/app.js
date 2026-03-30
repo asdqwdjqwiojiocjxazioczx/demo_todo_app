d56101be8f9bae3a9bcd01392fa40baa125dd9be
s <hungpd23800@gmail.com>
2026-03-30 16:46:42 +0700

    [FE] Build UI to create Todo and add comment

--- src/app.js
+++ src/app.js
@@ -15,6 +15,7 @@ const app = express();
 app.use(helmet());
 app.use(cors());
 app.use(express.json({ limit: '1mb' }));
+app.use(express.static('public'));
 app.use(cookieParser());
 app.use(morgan('dev'));
 
